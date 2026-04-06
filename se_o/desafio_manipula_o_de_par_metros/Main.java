e304ed2f5522a8d7b4de9af26d7ae79f144255c7
MarceloBoaria <>
2026-04-06 22:56:22 +0000

    Desafio: Manipulação de Parâmetros

---
+++ se_o/desafio_manipula_o_de_par_metros/Main.java
@@ -0,0 +1,19 @@
+package com.example;
+import java.util.Arrays;
+
+public class Main {
+    public static int[] manipulateArray(int[] array, int multiplyBy, int addValue) {
+        int[] result = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            result[i] = array[i] * multiplyBy + addValue;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        manipulateArray(new int[]{1, 2, 3}, 2, 1);
+        int[] transformed = manipulateArray(new int[]{1,2,3}, 2, 1);
+        System.out.println(Arrays.toString(transformed));
+
+    }
+}
